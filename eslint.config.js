990de891692f69232d8399c6b09aaae644170c28
Nikolay Belyaev <nikolay.belyaev@touchin.ru>
2025-04-06 17:49:55 +0300

    add linters

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,63 @@
 import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tsEslint from "typescript-eslint";
+import pluginImport from "eslint-plugin-import";
 import pluginReact from "eslint-plugin-react";
-
+import pluginReactHook from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tsEslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["*/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsEslint.parser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      import: pluginImport,
+      "unused-imports": pluginUnusedImports,
+        "react-hooks": pluginReactHook,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...tsEslint.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "import/order": [
+        "warn",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "unused-imports/no-unused-imports": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  eslintConfigPrettier
 ]);
